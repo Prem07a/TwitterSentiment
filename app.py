56a9ed5fd247fa559fdcd56e986ab62204bcc515
Prem Gaikwad <gen.premganeshgaikwad@gmail.com>
2023-12-20 00:42:52 +0530

    update

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def twitter_sentiment_analysis():
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 
     
     st.markdown(pdf_display, unsafe_allow_html=True)
