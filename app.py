9e65f833ee2bd7e15b801539469c4d02d64d6816
Prem Gaikwad <gen.premganeshgaikwad@gmail.com>
2023-12-20 00:42:11 +0530

    Changes

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ def predict_sentiment(text):
 # Define the pages
 def twitter_sentiment_analysis():
     st.title("Twitter Sentiment Analysis")
-    # Opening file from file path
+    
     with open('./SentimentAnalysis.html', "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    # Embedding PDF in HTML
+    
     pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
 
-    # Displaying File
+    
     st.markdown(pdf_display, unsafe_allow_html=True)
     
 def sentiment_check():
